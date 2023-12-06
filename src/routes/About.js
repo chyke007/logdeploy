fe52ec314e82107083ac7dc589321e699b5a9f4f
Chibuike Nwachukwu <nwachukwuchibuike2012@gmail.com>
2023-12-06 20:24:50 +0100

    Adds Github actions

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,5 +1,7 @@
+const apiKey = process.env.REACT_APP_API_KEY;
+
 const About = () => {
-      return <h1>About page</h1>
-}
+  return <h1>About page 1.0, Secret: {apiKey}</h1>;
+};
 
 export default About;
